465dfd20740c949222b47d32e224e7812987357d
kihernandez <khernandezeuceda@byupathway.edu>
2026-01-31 14:46:16 -0600

    Week 3 Assignment

--- server.js
+++ server.js
@@ -68,12 +68,11 @@ app.use(async (err, req, res, next) => {
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 3000
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`Server running on port ${port}`)
 })
